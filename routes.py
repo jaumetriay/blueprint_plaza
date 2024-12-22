6680c5c99571e99403b90167cd4c04f1212f041b
Dídac Gibert <80513402+didacgibert@users.noreply.github.com>
2024-12-22 18:54:49 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,5 +1,4 @@
 from flask import render_template, redirect, url_for, abort
-from utils.read_json import load_ads
 
 """
 Initialize routes for the Flask application.
@@ -25,23 +24,25 @@ def init_routes(app, cache):
     def index():
         return redirect(url_for('home'))
 
-    @cache.cached(timeout=3600)
-    def cached_load_ads():
-        return load_ads()
-
     @app.route('/home', strict_slashes=False, methods=['GET'])
     def home():
-        return render_template('home.html', ads=cached_load_ads())
+        return render_template('home.html')
 
-    @app.route('/ad/<int:ad_id>', strict_slashes=False, methods=['GET'])
-    def get_ad(ad_id):
-        ads = cached_load_ads()
-        ad = next((ad for ad in ads if ad['id'] == ad_id), None)
-        if ad:
-            return render_template('ad.html', ad=ad)
-        else:
-            return abort(404)
+
 
     @app.route('/about', strict_slashes=False, methods=['GET'])
     def about():
         return render_template('about.html')
+
+    @app.route('/proyecto1', strict_slashes=False, methods=['GET'])
+    def proyecto1():
+        return render_template('proyecto1.html')
+
+    @app.route('/proyecto2', strict_slashes=False, methods=['GET'])
+    def proyecto2():
+        return render_template('proyecto2.html')
+
+    @app.route('/proyecto3', strict_slashes=False, methods=['GET'])
+    def proyecto3():
+        return render_template('proyecto3.html')
+
